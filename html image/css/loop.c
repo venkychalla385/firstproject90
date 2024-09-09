4969694d393ec05697a18b051a47edd2e98a5d9a
venkychalla385 <challavenkey>
2024-09-09 20:44:49 +0530

    d

---
+++ html image/css/loop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+    int a;
+    a=9;
+    for(int i=1;i<=10;i++) {
+        printf("\n%d",a);
+        printf(/n);
+        printf("hello my name is venky");
+    }
+}
---
+++ html image/css/sm.cs
@@ -0,0 +1,17 @@
+* {
+    box-sizing:border-box;
+    margin:0px;
+    padding:0px;
+
+}
+.container {
+    width:100px;
+    height:100px;
+    background-color:black;
+    padding:10px;
+}
+.one{
+    width:100px;
+    height:100px;
+    background-color:yellow;
+}
